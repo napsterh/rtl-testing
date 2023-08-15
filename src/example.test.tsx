c9fc5db3676e09e40e11466f0bde3c13d7f11c58
Napster <rome.1205h@gmail.com>
2023-08-14 23:52:03 -0500

    update incode INE

---
+++ src/example.test.tsx
@@ -0,0 +1,21 @@
+import { sumPositiveNumber } from './example'
+
+describe('Cuando los argumentos que pasan son numeros positivos', () => {
+   test('Deberia retornar la respuesta correcta', () => {
+      expect(sumPositiveNumber(4,5)).toBe(9)
+   })
+})
+
+describe('Cuando uno de los argumentos es negativo', () => {
+   test('Deberia ser un error', () => {
+      let error
+      try {
+         sumPositiveNumber(-1,5)
+      } catch (err) {
+         error = err
+      }
+      expect(error).toBeDefined()
+      expect(error.message).toBe('One of number is negative')
+   })
+})
+
